6e99dc583cfec9588e341bc607f7e7eb33310da0
QuinceyLi <995918972@qq.com>
2024-08-25 14:38:22 +0800

    240825

---
+++ Hot100/Quincey/283.move-zeroes.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=283 lang=python3
+# @lcpr version=30204
+#
+# [283] 移动零
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        n_zero = 0
+        right = len(nums)
+        while left < right:
+            if nums[left] == 0:
+                while left+n_zero<right and nums[left+n_zero] == 0:
+                    n_zero += 1
+                for i in range(left+n_zero,right):
+                    nums[i-n_zero] = nums[i]
+                nums[right-n_zero:] = [0]*len(nums[right-n_zero:])
+                right -= n_zero
+                n_zero = 0
+            else:
+                left += 1
+                
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [0,1,0,3,12]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0]\n
+# @lcpr case=end
+
+#
+
