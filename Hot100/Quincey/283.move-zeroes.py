aabb8d07bc9516da79264b3262b86f5b4341c53b
Quincey Li <995918972@qq.com>
2024-08-25 14:57:48 +0800

    240825

--- Hot100/Quincey/283.move-zeroes.py
+++ Hot100/Quincey/283.move-zeroes.py
@@ -15,21 +15,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        n_zero = 0
-        right = len(nums)
-        while left < right:
-            if nums[left] == 0:
-                while left+n_zero<right and nums[left+n_zero] == 0:
-                    n_zero += 1
-                for i in range(left+n_zero,right):
-                    nums[i-n_zero] = nums[i]
-                nums[right-n_zero:] = [0]*len(nums[right-n_zero:])
-                right -= n_zero
-                n_zero = 0
+        # 我的解法
+        # left = 0
+        # n_zero = 0
+        # right = len(nums)
+        # while left < right:
+        #     if nums[left] == 0:
+        #         while left+n_zero<right and nums[left+n_zero] == 0:
+        #             n_zero += 1
+        #         for i in range(left+n_zero,right):
+        #             nums[i-n_zero] = nums[i]
+        #         nums[right-n_zero:] = [0]*len(nums[right-n_zero:])
+        #         right -= n_zero
+        #         n_zero = 0
+        #     else:
+        #         left += 1
+        left,right = 0,0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left+=1
+                right+=1
             else:
-                left += 1
-                
+                right+=1                
 # @lc code=end
 
 
