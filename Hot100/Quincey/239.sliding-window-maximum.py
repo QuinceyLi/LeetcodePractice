9fb3d2ea2a3996b7af2e7ba965645e36b93439d3
QuinceyLi <995918972@qq.com>
2024-10-22 21:40:08 +0800

    1022

---
+++ Hot100/Quincey/239.sliding-window-maximum.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=239 lang=python3
+# @lcpr version=30204
+#
+# [239] 滑动窗口最大值
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # 数据结构学的是一坨答辩
+        ans = []
+        q = deque()
+
+        for i,x in enumerate(nums):
+            while q and nums[q[-1]] <= x:
+                q.pop()
+            q.append(i)
+            while i - q[0] >= k:
+                q.popleft()
+            if i >= k-1:
+                ans.append(nums[q[0]])
+        return ans
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,3,-1,-3,5,3,6,7]\n3\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n1\n
+# @lcpr case=end
+
+#
+
